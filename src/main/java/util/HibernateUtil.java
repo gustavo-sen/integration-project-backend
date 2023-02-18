3150ecfd1364dd288ae4d794d5ed919cf5c64331
Gustavo Abdon <gustavoabdon2@gmail.com>
2023-02-18 13:23:54 -0300

    Singleton implemented

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -4,23 +4,28 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class HibernateUtil {
+// final classes are made to not be extended or inherited
+public final class HibernateUtil {
 
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory sessionFactory;
 
+    //singleton criado e verificando se a sessão existe, caso não exista ele cria se n ele retorna
     private static SessionFactory buildSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory();
 
-        }
-        catch (Throwable ex) {
+        if(sessionFactory == null){
+            try {
+                // Create a SessionFactory from hibernate.cfg.xml
+                return new Configuration().configure().buildSessionFactory();
+            }
+            catch (Throwable ex) {
 
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
+                System.err.println("Initial SessionFactory creation failed." + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+        }else {
+            return getSessionFactory();
         }
     }
-
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
