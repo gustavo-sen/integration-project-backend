2a7ff1b891622afab93bbfed72c441f007121539
gustavoabdon <gustavoabdon2@gmail.com>
2023-03-15 17:20:24 -0300

    lombok

--- src/main/java/com/eletra/product/apirest/model/CategoryEntity.java
+++ src/main/java/com/eletra/product/apirest/model/CategoryEntity.java
@@ -1,17 +1,11 @@
 package com.eletra.product.apirest.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "categories_table")
-@Getter
-@Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class CategoryEntity {
 
